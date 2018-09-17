ecc63bb0da336001d30d4828a9e07f1100d783dc
Moonstone <justwe77@163.com>
2018-09-17 23:05:01 +0800

    make plan

--- vport1.0/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import IRoute from './router'
-import './style/common.less'
-
-ReactDOM.render(<IRoute />, document.getElementById('root'));
-
--- vport1.0/src/pages/nomatch/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default class NoMatch extends React.Component{
-    render(){
-        return(
-            <div style={{textAlign:'center', fontSize: '20px'}}>404 Not Found.</div>
-        )
-    }
-}
